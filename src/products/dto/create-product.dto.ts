0d36154b4f2b9925dea92e5592a921d14e397873
Alexis Duque <ing.alexis.duque@gmail.com>
2024-01-29 08:34:12 -0500

    conection db atlas

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { MESSAGE_RETURN } from '../../utils/_messages';
 import {
   IsString,
@@ -7,14 +8,28 @@ import {
 
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Nombre del producto',
+    type: String,
+  })
     @IsNotEmpty({ message: MESSAGE_RETURN.required })
   @IsString({ message: MESSAGE_RETURN.string })
   readonly name: string;
 
+  @ApiProperty({
+    description: 'precio del producto',
+    type: Number,
+  })
   @IsNotEmpty({ message: MESSAGE_RETURN.required })
   @IsNumber()
   readonly price: number;
 
+  @ApiProperty({
+    description: 'Tipo de producto puede usar esats dos opciones "PERISHABLE", "NONPERISHABLE"',
+    type: Number,
+  })
   @IsString({ message: MESSAGE_RETURN.required })
   readonly type: string;
+
+ 
 }
