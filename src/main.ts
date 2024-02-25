575c27089b218d01ef23fff67218e96a04a961bd
Alexis Duque <ing.alexis.duque@gmail.com>
2024-02-25 12:18:06 -0500

    config cors and vercel for deploy prd

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(
+    {
+      origin: [
+        "https://store-products-front-afcdneoi3-alexis-duques-projects.vercel.app"
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      
+  }
+  );
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
